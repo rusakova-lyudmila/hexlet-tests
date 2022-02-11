0eca2a0a3c4254eca1aa4e94e4eb88345800f4ea
Rusakova Lyudmila <l.rusakova2108@gmail.com>
2022-02-11 20:47:25 +0500

    feat: use assert for testing

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,6 @@
+import { strict as assert } from 'assert';
 import capitalize from '../src/capitalize.js';
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
+assert.equal(capitalize('hello'), 'Hello');
 
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
-
-console.log('Все тесты пройдены!');
+assert.equal(capitalize(''), '');
